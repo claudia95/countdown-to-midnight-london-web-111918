62781ff228b4da525dde380adc06e11b265cd6e9
Claudia Fox <claudiafox95@icloud.com>
2018-11-11 21:00:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ def countdown(integer)
   return "HAPPY NEW YEAR!" 
 end
 
+def countdown_with_sleep(integer) 
+  while integer > 0 
+    puts "#{integer} SECOND(S)!"
+    sleep(1)
+    integer -= 1 
+  end
+  "HAPPY NEW YEAR!" 
+end
